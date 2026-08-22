835692b07c4857527cc4593939d07be3bbef8605
Fagdlacerda <fagdlacerda@gmail.com>
2026-08-22 11:08:08 -0300

    sla commit

---
+++ Geo.h
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Retorna o centroide de um polígono. Os vértices devem estar em ordem anti-horária.
+pt centroid(const vector<pt>& v) {
+    ld area = 0, cx = 0, cy = 0;
+    int n = v.size();
+    
+    for (int i = 0; i < n; i++) {
+        int j = (i + 1) % n; // O próximo vértice (circular)
+        
+        // Produto vetorial da aresta (2x a área do triângulo com a origem)
+        ld cross = (v[i].x * v[j].y - v[j].x * v[i].y); 
+        
+        area += cross;
+        cx += (v[i].x + v[j].x) * cross;
+        cy += (v[i].y + v[j].y) * cross;
+    }
+    
+    area /= 2.0;
+    cx /= (6.0 * area);
+    cy /= (6.0 * area);
+    
+    return pt(cx, cy);
+}
+
+struct CentroidQueries {
+    int n;
+    vector<pt> v;
+    vector<__int128_t> pref_area, pref_cx, pref_cy;
+
+    // Função 1: Recebe o polígono (0-based) e pré-calcula os prefixos
+    CentroidQueries(const vector<pt>& poly) {
+        n = poly.size();
+        
+        // Ajusta tamanho para 2N + 1 para lidar com queries circulares e ser 1-based
+        v.resize(2 * n + 1);
+        for (int i = 0; i < n; i++) {
+            v[i + 1] = poly[i];
+            v[i + n + 1] = poly[i]; // Duplica
+        }
+
+        pref_area.assign(2 * n + 1, 0);
+        pref_cx.assign(2 * n + 1, 0);
+        pref_cy.assign(2 * n + 1, 0);
+
+        // Constrói os prefixos começando de i=2 (primeira aresta válida)
+        for (int i = 2; i <= 2 * n; i++) {
+            long long cross = sarea2(pt(0, 0), v[i-1], v[i]);
+            
+            pref_area[i] = pref_area[i-1] + (__int128_t)cross;
+            pref_cx[i]   = pref_cx[i-1]   + (__int128_t)(v[i-1].x + v[i].x) * cross;
+            pref_cy[i]   = pref_cy[i-1]   + (__int128_t)(v[i-1].y + v[i].y) * cross;
+        }
+    }
+
+    // Função 2: Responde a query das coordenadas do centroide
+    // a e b são os índices dos vértices do corte (1-based, de 1 a N)
+    pair<long double, long double> query(int a, int b) {
+        if (a > b) b += n;
+
+        // Calcula a aresta de fechamento (de b para a)
+        long long cross_corte = sarea2(pt(0, 0), v[b], v[a]);
+        __int128_t cx_corte = (__int128_t)(v[b].x + v[a].x) * cross_corte;
+        __int128_t cy_corte = (__int128_t)(v[b].y + v[a].y) * cross_corte;
+
+        // Soma as bordas com o corte
+        __int128_t area_total = (pref_area[b] - pref_area[a]) + cross_corte;
+        __int128_t cx_total = (pref_cx[b] - pref_cx[a]) + cx_corte;
+        __int128_t cy_total = (pref_cy[b] - pref_cy[a]) + cy_corte;
+
+        // Calcula as coordenadas finais (dividindo por 3 * area porque cross = 2 * area)
+        long double ans_x = (long double)cx_total / (long double)(3 * area_total);
+        long double ans_y = (long double)cy_total / (long double)(3 * area_total);
+
+        return {ans_x, ans_y};
+    }
+};
--- String.h
+++ String.h
@@ -12,6 +12,32 @@ vector<int> get_pref(string const& s) {
     }
     return pi;
 }
+
+vector<vector<int>> dfa(string const& s){
+    int n = s.size();
+    vector<int> pi(n);
+    for (int i = 1; i < n; i++) {
+        int j = pi[i-1];
+        while (j > 0 && s[i] != s[j]) j = pi[j-1];
+        if (s[i] == s[j]) j++;
+        pi[i] = j;
+    }
+    
+    vector<vector<int>> nxt(n+1, vector<int>(26));
+    for(int i=0; i<=n; i++){
+
+        for(int c=0; c<26; c++){
+            if(i<n && (s[i]-'a') == c) {
+                nxt[i][c] = i+1; // A letra bateu certo
+            }
+            else if(i>0){
+                nxt[i][c] = nxt[pi[i-1]][c];
+            }
+            else nxt[i][c] = 0;
+        }
+    }
+    return nxt;
+}
  
 vector<int> get_z(string s) {
 	int n = s.size();
